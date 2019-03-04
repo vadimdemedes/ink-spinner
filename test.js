b30ce178030e0c621912ae7648c38adb8a689d4a
Vadim Demedes <vdemedes@gmail.com>
2019-03-03 23:57:53 -0800

    Migrate to Ink 2 (#2)

--- test.js
+++ test.js
@@ -1,62 +1,36 @@
-import {h, build, Color} from 'ink';
-import renderToString from 'ink/lib/render-to-string';
-import {stub} from 'sinon';
+import EventEmitter from 'events';
+import React from 'react';
+import {render} from 'ink';
+import {spy} from 'sinon';
 import spinners from 'cli-spinners';
 import test from 'ava';
+import delay from 'delay';
 import Spinner from '.';
 
-test('render', t => {
-	let component;
+test('render spinner', async t => {
+	const stdout = {
+		columns: 100,
+		write: spy()
+	};
 
-	function setRef(ref) {
-		component = ref;
-	}
-
-	const spinner = spinners.dots;
-	let tree;
-
-	for (let frame = 0; frame < spinner.frames.length; frame++) {
-		tree = build(<Spinner ref={setRef}/>, tree);
-		t.is(renderToString(tree), spinner.frames[frame]);
-
-		component.setState({
-			frame: frame + 1
-		});
-	}
-});
-
-test('pass props to <Color>', t => {
-	const spinner = spinners.dots;
-
-	const actual = build(<Spinner green/>);
-	const expected = build(<Color green>{spinner.frames[0]}</Color>);
-
-	t.is(renderToString(actual), renderToString(expected));
-});
-
-test('spin', t => {
-	stub(Spinner.prototype, 'setState');
-
-	let component;
-
-	function setRef(ref) {
-		component = ref;
-	}
-
-	build(<Spinner ref={setRef}/>);
+	const stdin = new EventEmitter();
+	stdin.setRawMode = () => {};
+	stdin.setEncoding = () => {};
+	stdin.pause = () => {};
 
 	const spinner = spinners.dots;
+	const app = render(<Spinner/>, {
+		stdout,
+		stdin,
+		debug: true
+	});
 
-	let frame = 1;
-	let i = 0;
+	await delay(spinner.frames.length * spinner.interval);
+	app.unmount();
 
-	while (i < spinner.frames.length * 2) {
-		component.switchFrame();
+	const allFrames = stdout.write.args.map(args => args[0]);
+	const frames = [...new Set(allFrames)];
 
-		t.deepEqual(component.setState.getCall(i).args[0], {frame});
-		component.state = {frame};
-
-		frame = frame === spinner.frames.length - 1 ? 0 : frame + 1;
-		i++;
-	}
+	t.deepEqual(frames, spinner.frames);
+	t.pass();
 });
